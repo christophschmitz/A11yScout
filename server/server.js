33d8fb839968ac7d17b29ae3ad3d01a16c1c2fff
Christoph Schmitz <c.schmitz@outlook.de>
2023-11-07 17:56:35 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path'); // Fügen Sie diese Zeile hinzu, um das 'path' Modul zu importieren
 const tagsRouter = require('./routes/tagsRouter');
 const resourcesRouter = require('./routes/resourcesRouter'); // Importieren Sie den Ressourcen-Router, wenn Sie ihn erstellt haben
 
@@ -17,16 +18,23 @@ mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log('Datenbankverbindung erfolgreich aufgebaut'))
-  .catch(err => console.error('Fehler beim Verbinden mit der Datenbank:', err));
-
-app.get('/', (req, res) => {
-  res.send('Hallo, das ist mein Server!');
-});
+.then(() => console.log('Datenbankverbindung erfolgreich aufgebaut'))
+.catch(err => console.error('Fehler beim Verbinden mit der Datenbank:', err));
 
 // Routen
-app.use('/api/tags', tagsRouter);  // Alle Tag-bezogenen Routen werden im tagsRouter behandelt
-app.use('/api/resources', resourcesRouter);  // Hiermit integrieren wir die Ressourcen-Routen
+app.use('/api/tags', tagsRouter); // Alle Tag-bezogenen Routen werden im tagsRouter behandelt
+app.use('/api/resources', resourcesRouter); // Hiermit integrieren wir die Ressourcen-Routen
+
+// Nach allen anderen Middleware und Routing
+if (process.env.NODE_ENV === 'production') {
+  // Express wird Produktions-Assets bereitstellen
+  app.use(express.static('build'));
+
+  // Express wird die index.html-Datei bereitstellen, wenn es die Route nicht erkennt
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 
 // Starte den Server
 const PORT = process.env.PORT || 5090;
